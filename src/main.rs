119a7611887df84214f1d2b2ce5d8a50e1c509ac
Ricardo Casía <ricardocasia.dev@gmail.com>
2023-05-17 21:13:34 +0200

    only accepts 'message' or nothing as third argument

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,25 @@
 mod extract_ticket_from_branch;
 
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    // only accept 'message' or nothing as argument[2]
+    if let Some(commit_source) = args.get(2) {
+        if commit_source != "message" {
+            println!("Type of the commit should be 'message'");
+            return;
+        }
+    }
+    
+    for idx in 0..args.len() {
+        println!("arg[{}]: {}", idx, args[idx]);
+    }
 }
 
 
+#[cfg(test)]
+mod tests {
 
+}
